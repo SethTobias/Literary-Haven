f5e20760915a7bf178338cb974f00d4638da0cb2
SethTobias <seth.addams1107@gmail.com>
2023-12-06 15:47:58 +0200

    fixed conflicts

--- JavaScript/product.js
+++ JavaScript/product.js
@@ -1,42 +1,59 @@
 //
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
+    //
     document.querySelector(".loader.center").style.display = "none";
+    //
     document.querySelector("main").style.display = "block";
   }, 2000);
 });
 
 // Declaring main tag as a variable
-const mainContent = document.querySelector("main");
+const mainContent = document.querySelector(".mainContent");
 
 //
 let books = JSON.parse(localStorage.getItem("books"));
 
-let purchased = [];
 //
-let products = books.map(function (item, index) {
-  return `
-                <div>
-                    ${index}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    $<img src="${item.url}">
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                    ${item.name}
-                </div>
-              `;
-});
+let purchased = [];
 
-// cars.sort(function (a, b) {
-//   return a.year - b.year;
-// });
-// displayCars();
+//
 
-mainContent.innerHTML = products.join("");
+//
+const searchBar = document.querySelector(".search");
+
+//
+function showBooks(books) {
+  mainContent.innerHTML = books
+    .map(
+      (book, index) => `
+    <div>
+      ${index + 1}. ${book.name}
+      <img src="${book.url}">
+    </div>
+  `
+    )
+    .join("");
+}
+
+//
+function searchItem() {
+  let searchValue = searchBar.value.toLowerCase();
+
+  if (searchValue !== "") {
+    const filteredBooks = books.filter((book) =>
+      book.name.toLowerCase().includes(searchValue)
+    );
+    showBooks(filteredBooks);
+  } else {
+    mainContent.innerHTML = `
+    <div>
+      <h1 class="display-1 text-danger text-center">Item Was Not Found?</h1>
+    </div>
+  `;
+  }
+}
+
+searchBar.addEventListener("input", searchItem);
+
+showBooks(books);
