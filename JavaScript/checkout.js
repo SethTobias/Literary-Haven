8511a8727241f9795b73c11251ab4509626508ff
SethTobias <seth.addams1107@gmail.com>
2023-12-08 12:13:07 +0200

    ?

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -1,8 +1,18 @@
 //
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    //
+    document.querySelector(".loader.center").style.display = "none";
+    //
+    document.querySelector("main").style.display = "block";
+  }, 2000);
+});
+
+//
 let cart = JSON.parse(localStorage.getItem("cart"));
 
 //
-const mainContent = document.querySelector("ul");
+const mainContent = document.querySelector("tbody");
 
 //
 const purchasedContent = document.querySelector("main");
@@ -16,11 +26,25 @@ function updateLocalCart() {
 //
 window.onload = function displayCart() {
   let purchasedItems = cart.map(function (cart, index) {
-    return `
-           <li>${
-             index + 1
-           } ${cart.name} ${cart.volume} ${cart.price} <button class="removeItem" value="${index}">X</button></li>
-        `;
+    if (mainContent == "") {
+      return `
+        <tr>
+          <td colspan="7"><h1 class="display-1 text-info">No items in Cart:</h1></td>
+        </tr>
+      `;
+    } else {
+      return `
+      <tr>
+      <td style="width:5%;">${cart.name}</td>
+      <td style="width:5%;">Volume ${cart.volume}</td>
+      <td style="width:5%;">${cart.type}</td>
+      <td style="width:12.5%;"><img src=${cart.url}></td>
+      <td style="width:5%;">${cart.quantity}</td>
+      <td style="width:5%;">R${cart.price * cart.quantity}</td>
+      <td style="width:5%;"><button class="removeItem" value="${index}">X</button></td>
+      </tr>
+      `;
+    }
   });
   function removeItem(index) {
     cart.splice(index, 1);
@@ -48,6 +72,7 @@ function confirmedPurchase() {
   console.log("Thank you for your purchase! It is much appreciated!!!");
   purchasedContent.innerHTML = `<h1 class="text-center text-danger display-1">Thank you for your purchase! It is much appreciated!!!</h1>`;
   updateLocalCart();
+  setTimeout(displayCart(cart), 1000);
 }
 
 //
